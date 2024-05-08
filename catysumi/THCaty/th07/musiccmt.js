9f593e0eaef0c2d54e577814fb5fbaf2c85447c7
CatyYafumo <catyyafumo@github.com>
2024-05-08 16:45:45 -0400

    THCaty
    added themes description & name

--- catysumi/THCaty/th07/musiccmt.js
+++ catysumi/THCaty/th07/musiccmt.js
@@ -1,17 +1,31 @@
 {
-    "17": [
+    "15": [
         "@",
-        "Tema de la stage Profanación.",
+        "Tema de la stage Destrucción.",
         "Conocí el calamity por este tema y me sigue pareciendo epico.",
         "Me pareció una buena opción usarlo en esta oportunidad."
     ],
+    "16": [
+        "@",
+        "Tema de Wendy.",
+        "Honestamente me quedé sin ideas.",
+        "Estaba por poner un tema de Gloryhammer pero sentí que ninguno quedaba.",
+        "Pero como ella dice: \"Nos ayudamos mutuamente\"",
+        "Si nos ponemos a pensar los \"Guardiaes\" son cada uno Shikigami del otro."
+    ],
+    "17": [
+        "@",
+        "Tema de la stage Profanación.",
+        "La palabra misma te hace sentir que el jefe es muy poderoso.",
+        "Stained, Brutal Calamity (Grief) siempre me hace sentir PODER."
+    ],
     "18": [
         "@",
-        "Tema de Caty Yafumo.",
-        "El metal transmite distintas emociones.",
-        "En este caso quería que esta pelea se sintiera intensa, frenética y llena de poder.",
-        "¡Y el power metal es perfecto!",
-        "Además la canción puede facilmente estar en sintonía con la pelea.",
-        "sisoy."
+        "Tema de Caty.",
+        "Un metalero puede parecer rudo pero es gentil y carismático.",
+        "Eso describe al personaje perfectamente. Además.",
+        "Caty acaba sobre todo con las divinidades siendo solo humana.",
+        "¡Que profano! El metal causo controversias por la religión",
+        "y ella es muy poderosa. Por eso Ensiferum, Power Metal, PERFECT."
     ]
 }
--- catysumi/THCaty/themes.js
+++ catysumi/THCaty/themes.js
@@ -1,4 +1,6 @@
 {
-    "th07_17": "Raw, Unfiltered Calamity",
+    "th07_15": "Raw, Unfiltered Calamity",
+    "th07_16": "An Ilusory Maiden's Funeral - Necro-Fantasy",
+    "th07_17": "Stained, Brutal Calamity",
     "th07_18": "Rum, Woman, Victory"
 }
