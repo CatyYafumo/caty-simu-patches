cb227e8fb2cbb108970a1b998af8fb7e9fbf7ad5
CatyYafumo <adelgadocuwu@gmail.com>
2025-10-12 12:41:11 -0300

    ULTRA ANY RADIANCE 15.0 ADDON
    YES! Another addon for LoEK!
    Makes the Radiance absurdely hard lmao.

---
+++ catysumi/UARadiance_15/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "catysumi/catysumi/loek"
+  ],
+  "id": "UARadiance_15",
+  "servers": [
+    "https://mirrors.thpatch.net/catysumi/catysumi/uaradiance15/"
+  ],
+  "title": "(TH15) Touhou X Hollow Knight Addon: Ultra Any Radiance 15.0",
+  "update": true
+}
