79c1b62ea753825e8a9fd76511c4e06f8750b313
CatyYafumo <adelgadocuwu@gmail.com>
2026-06-07 15:38:02 -0400

    LGW 1.0
    POGGERS POGGERS

--- catysumi/repo.js
+++ catysumi/repo.js
@@ -16,6 +16,7 @@
         "CD": "(TH11) Touhou Calamity ~ Calamitous Danmaku (Terraria Calamity mod)",
         "HKPoL": "(TH15) Touhou x Hollow Knight: Pantheon of Legacy Addon. Make extra stage a bossrush like Hollow Knight Pantheons. Needs LoEK!",
         "JoMAD": "(TH19) Touhou Universe ~ Journey of Multiple Apocaliptic Dimensions",
-        "LGW": "(TH17) (DEMO) Touhou Tsuneriuketsu ~ Legendary God War"
+        "LGW": "(TH17) Touhou Tsuneriuketsu ~ Legendary God War",
+        "PtGO": "(TH20) (DEMO) Touhou Makakushin ~ Peace to Gensokyo's Order"
     }
 }
