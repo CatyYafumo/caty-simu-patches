1511212b464194b7a29bfb66d57a0cdbc6cdb7d3
CatyYafumo <adelgadocuwu@gmail.com>
2026-07-23 20:58:21 -0400

    PtGO DEMO update
    - REBALANCED EASY AND NORMAL MODE STAGE 1
    - REBALANCED EASY AND NORMAL MODE STAGE 2 (bosses to do)
    - Better soul enemies bullets
    - Compressed some stuff into anm files
    - Deleted unused content
    - Better dialogs

--- catysumi/PtGO/th20/stonetext.js
+++ catysumi/PtGO/th20/stonetext.js
@@ -1,90 +1,508 @@
 {
     "0": "Eternal Void",
+    "0_0": [
+        "A stone made by the Void Guardian, strong as indestructible.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Great Void Stabbing",
+        "Kills anything at its front."
+    ],
     "1": "Eternal Void",
+    "1_0": [
+        "Unfocused Shot: Void Sword",
+        "",
+        "Strong, limited-area swords."
+    ],
     "2": "Eternal Void",
+    "2_0": [
+        "Focused Shot: Void Sword",
+        "",
+        "Strong, limited-area swords."
+    ],
     "3": "Eternal Void",
+    "3_0": "Buff: Your strenght is empowered while focused.",
     "4": "Divine Echo",
+    "4_0": [
+        "A stone made by the Void Guardian, contains a bit of divine power.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Emptiness Retaliation",
+        "Shoots destructive void blasts in a wide-area."
+    ],
     "5": "Divine Echo",
+    "5_0": [
+        "Unfocused Shot: Cursed Ink",
+        "",
+        "Dark blasts at any point of your view."
+    ],
     "6": "Divine Echo",
+    "6_0": [
+        "Focused Shot: Cursed Ink",
+        "",
+        "Dark blasts at any point of your view."
+    ],
     "7": "Divine Echo",
+    "7_0": "Buff: Guardian Attack Gauge fills up faster.",
     "8": "Soul Catalyst",
+    "8_0": [
+        "A stone made by the Eternity Guardian, manifestates lifeforce.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Infinite Homming Whip",
+        "A whip that will never let it's target go."
+    ],
     "9": "Soul Catalyst",
+    "9_0": [
+        "Unfocused Shot: Whip Spirit",
+        "",
+        "Homing, infinite parts of the weapon."
+    ],
     "10": "Soul Catalyst",
+    "10_0": [
+        "Unfocused Shot: Whip Spirit",
+        "",
+        "Homing, infinite parts of the weapon."
+    ],
     "11": "Soul Catalyst",
+    "11_0": "Buff: Your strenght is empowered while unfocused.",
     "12": "Starlight Core",
+    "12_0": [
+        "Filled with the madness hidden behind all seasons.",
+        "Surely this concealed power will tear open a hole in the sanctuary.",
+        "",
+        "Abnormal Attack: Reliable God's Back",
+        "Eliminates enemy bullets behind you."
+    ],
     "13": "Starlight Core",
+    "13_0": [
+        "Unfocused Shot: Power of the Back",
+        "",
+        "Aims shots at enemies behind the player options."
+    ],
     "14": "Starlight Core",
+    "14_0": [
+        "Focused Shot: Power of the Back",
+        "",
+        "Aims shots at enemies behind the player options."
+    ],
     "15": "Starlight Core",
+    "15_0": "Support Effect: Power items drop more often.",
     "16": "Unholy Thunder",
+    "16_0": [
+        "The repulsive power keeping everything with the unholy energy",
+        "sealed within the earth will surely open the sanctuary's doors.",
+        "",
+        "Abnormal Attack: Overflowing Taboo",
+        "Applies a barrier to prevent being hit for a moderate time."
+    ],
     "17": "Unholy Thunder",
+    "17_0": [
+        "Unfocused Shot: Taboo Option",
+        "",
+        "The option itself can attack and shoots directly."
+    ],
     "18": "Unholy Thunder",
+    "18_0": [
+        "Focused Shot: Taboo Option",
+        "",
+        "Options with attack power are concentrated into charms."
+    ],
     "19": "Unholy Thunder",
-    "20": "Pure Blaze",
-    "21": "Pure Blaze",
-    "22": "Pure Blaze",
-    "23": "Pure Blaze",
+    "19_0": "Support Effect: Grazing enemy bullets sometimes deletes them.",
+    "20": "Impure Blaze",
+    "20_0": [
+        "A stone made by the Profaner Guardian, it profanes even the most perfect.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Purge of the Holy",
+        "Whatever it happens, all bullets convert into blazing counterattack arrows."
+    ],
+    "21": "Impure Blaze",
+    "21_0": [
+        "Unfocused Shot: Profanation Arrows",
+        "",
+        "Your orbs destroy bullets it collides with and counterattacks."
+    ],
+    "22": "Impure Blaze",
+    "22_0": [
+        "Focused Shot: Profanation Arrows",
+        "",
+        "Your orbs destroy bullets it collides with and counterattacks."
+    ],
+    "23": "Impure Blaze",
+    "23_0": "Buff: If a bullet hits your orbs, they are sometimes destroyed.",
     "24": "Otherworldly Shade",
+    "24_0": [
+        "The impermanent power of beasts with the mercilessness of that ",
+        "vast, unending Hell will surely tear a hole into the sanctuary.",
+        "",
+        "Abnormal Attack: The Spider's Thread",
+        "Not getting hit while the attack is active yields a bomb fragment."
+    ],
     "25": "Otherworldly Shade",
+    "25_0": [
+        "Unfocused Shot: Steller's Sea Eagle Flock",
+        "",
+        "Releases a flock of eagles directly in front of you."
+    ],
     "26": "Otherworldly Shade",
+    "26_0": [
+        "Focused Shot: Steller's Sea Eagle Flock",
+        "",
+        "Releases a flock of eagles directly in front of you."
+    ],
     "27": "Otherworldly Shade",
+    "27_0": "Support Effect: Increases attack power against Wonder Stones.",
     "28": "Mystic Mana",
+    "28_0": [
+        "The gods' arcane power, filled with the force of the winds blowing ",
+        "through Youkai Mountain, will surely tear a hole into the sanctuary.",
+        "",
+        "Abnormal Attack: Arcane Spirit Strike",
+        "Unleashes a spiritual attack that releases offensive power and destroys enemy bullets."
+    ],
     "29": "Mystic Mana",
+    "29_0": [
+        "Unfocused Shot: Arcane Serpent",
+        "",
+        "Releases a snake that attacks the enemy."
+    ],
     "30": "Mystic Mana",
+    "30_0": [
+        "Focused Shot: Arcane Serpent",
+        "",
+        "Releases a snake that attacks the enemy."
+    ],
     "31": "Mystic Mana",
+    "31_0": "Support Effect: Increases the duration of Abnormal Attacks.",
     "32": "Stone Debris",
+    "32_0": [
+        "Absorbs the magical power of and mimics the Main Guardian Stone.",
+        "* Slightly less effective than the actual thing."
+    ],
     "33": "Stone Debris",
+    "33_0": [
+        "Mimics the Main Guardian Stone.",
+        "* Slightly less effective than the actual thing."
+    ],
     "34": "Stone Debris",
+    "34_0": [
+        "Mimics the Main Guardian Stone.",
+        "* Slightly less effective than the actual thing."
+    ],
     "35": "Stone Debris",
+    "35_0": [
+        "Mimics the Main Guardian Stone.",
+        "* Slightly less effective than the actual thing."
+    ],
     "36": "Eternal Void",
+    "36_0": [
+        "A stone made by the Void Guardian, strong as indestructible.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Pure Void Aura",
+        "Strong unstoppable void lasers."
+    ],
     "37": "Eternal Void",
+    "37_0": [
+        "Eternal Void: Void-casting Laser",
+        "",
+        "High-power laser for a mid-area use."
+    ],
     "38": "Eternal Void",
+    "38_0": [
+        "Eternal Void: Void-casting Laser",
+        "",
+        "High-power laser for a mid-area use."
+    ],
     "39": "Eternal Void",
+    "39_0": "Buff: Your strenght is empowered while focused.",
     "40": "Divine Echo",
+    "40_0": [
+        "A stone made by the Void Guardian, contains a bit of divine power.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Perfect Void Weapon",
+        "Empowered void swords cappable of destroy anything."
+    ],
     "41": "Divine Echo",
+    "41_0": [
+        "Unfocused Shot: Bloodstained Missile",
+        "",
+        "A high-power, extensive carpet bombing."
+    ],
     "42": "Divine Echo",
+    "42_0": [
+        "Focused Shot: Bloodstained Missile",
+        "",
+        "A high-power, extensive carpet bombing."
+    ],
     "43": "Divine Echo",
+    "43_0": "Buff: Guardian Attack Gauge fills up faster.",
     "44": "Soul Catalyst",
+    "44_0": [
+        "A stone made by the Eternity Guardian, manifestates lifeforce.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Whip of Eternity",
+        "A whip that surrounds your enemy."
+    ],
     "45": "Soul Catalyst",
+    "45_0": [
+        "Unfocused Shot: Icicle Shot",
+        "",
+        "A small ice shot that tends to attack things in front of you."
+    ],
     "46": "Soul Catalyst",
+    "46_0": [
+        "Focused Shot: Icicle Shot",
+        "",
+        "A small ice shot that tends to attack things in front of you."
+    ],
     "47": "Soul Catalyst",
+    "47_0": "Buff: Your strenght is empowered while unfocused.",
     "48": "Starlight Core",
+    "48_0": [
+        "Filled with the madness hidden behind all seasons.",
+        "Surely this concealed power will tear open a hole in the sanctuary.",
+        "",
+        "Abnormal Attack: Reliable God's Back",
+        "Eliminates enemy bullets behind you."
+    ],
     "49": "Starlight Core",
+    "49_0": [
+        "Unfocused Shot: A World With Neither Front nor Back",
+        "",
+        "Shoots into those probable blind spots behind you."
+    ],
     "50": "Starlight Core",
+    "50_0": [
+        "Focused Shot: A World With Neither Front nor Back",
+        "",
+        "Shoots into those probable blind spots behind you."
+    ],
     "51": "Starlight Core",
+    "51_0": "Support Effect: Power items drop more often.",
     "52": "Unholy Thunder",
+    "52_0": [
+        "The repulsive power keeping everything with the unholy energy",
+        "sealed within the earth will surely open the sanctuary's doors.",
+        "",
+        "Abnormal Attack: Overflowing Taboo",
+        "Applies a barrier to prevent being hit for a moderate time."
+    ],
     "53": "Unholy Thunder",
+    "53_0": [
+        "Unfocused Shot: Taboo Option",
+        "",
+        "The option creates a wall of offensive force."
+    ],
     "54": "Unholy Thunder",
+    "54_0": [
+        "Unfocused Shot: Taboo Option",
+        "",
+        "The option creates a wall of offensive force."
+    ],
     "55": "Unholy Thunder",
+    "55_0": "Support Effect: Grazing enemy bullets sometimes deletes them.",
     "56": "Pure Blaze",
+    "56_0": [
+        "A stone made by the Profaner Guardian, it profanes even the most perfect.",
+        "It will be the best weapon for this moment for sure.",
+        "",
+        "Power Release: Impious Desolation",
+        "Whatever it happens, all bullets convert into blazing counterattack arrows."
+    ],
     "57": "Pure Blaze",
+    "57_0": [
+        "Focused Shot: Blazing Arrows",
+        "",
+        "Your orbs destroy bullets it collides with and counterattacks."
+    ],
     "58": "Pure Blaze",
+    "58_0": [
+        "Focused Shot: Blazing Arrows",
+        "",
+        "Your orbs destroy bullets it collides with and counterattacks."
+    ],
     "59": "Pure Blaze",
+    "59_0": "Buff: If a bullet hits your orbs, they are sometimes destroyed.",
     "60": "Otherworldly Shade",
+    "60_0": [
+        "The impermanent power of beasts with the mercilessness of that ",
+        "vast, unending Hell will surely tear a hole into the sanctuary.",
+        "",
+        "Abnormal Attack: The Spider's Thread",
+        "Not getting hit while the attack is active yields a bomb fragment."
+    ],
     "61": "Otherworldly Shade",
+    "61_0": [
+        "Unfocused Shot: Wolf Pack",
+        "",
+        "Releases a pack of wolves directly in front of you."
+    ],
     "62": "Otherworldly Shade",
+    "62_0": [
+        "Focused Shot: Wolf Pack",
+        "",
+        "Releases a pack of wolves directly in front of you."
+    ],
     "63": "Otherworldly Shade",
+    "63_0": "Support Effect: Increases attack power against Wonder Stones.",
     "64": "Mystic Mana",
+    "64_0": [
+        "The gods' arcane power, filled with the force of the winds blowing ",
+        "through Youkai Mountain, will surely tear a hole into the sanctuary.",
+        "",
+        "Abnormal Attack: Arcane Spirit Strike",
+        "Unleashes a spiritual attack that releases offensive power and destroys enemy bullets."
+    ],
     "65": "Mystic Mana",
+    "65_0": [
+        "Unfocused Shot: Indigenous Frog",
+        "",
+        "Releases explosive frogs."
+    ],
     "66": "Mystic Mana",
+    "66_0": [
+        "Focused Shot: Indigenous Frog",
+        "",
+        "Releases explosive frogs."
+    ],
     "67": "Mystic Mana",
+    "67_0": "Support Effect: Increases the duration of Abnormal Attacks.",
     "68": "Stone Debris",
+    "68_0": "Absorbs the magical power of and mimics the Main Guardian Stone.",
     "69": "Stone Debris",
+    "69_0": "Mimics the Main Guardian Stone.",
     "70": "Stone Debris",
+    "70_0": "Mimics the Main Guardian Stone.",
     "71": "Stone Debris",
+    "71_0": [
+        "Mimics the Main Guardian Stone.",
+        "* Slightly less effective than the actual thing."
+    ],
     "72": "Eternal Void",
+    "72_0": [
+        "From her research into the Incident Stones,",
+        "Patchouli gave Reimu a prophecy-like warning.",
+        "\"Based on the nature of the pyramid,",
+        "a pseudo-omnipotent youkai will appear there.\"",
+        "Reimu headed for the pyramid, just in case."
+    ],
     "73": "Divine Echo",
+    "73_0": [
+        "The sculptor goddess, Keiki, conveyed her concerns to Reimu.",
+        "\"That pyramid might give birth to a false creator deity.",
+        "That deity will surely cause chaos on the surface world.\"",
+        "Reimu headed for the pyramid, just in case."
+    ],
     "74": "Soul Catalyst",
+    "74_0": [
+        "According to Yuyuko, time stands still in the realm of the dead.",
+        "However, time appears to be passing abnormally inside the pyramid.",
+        "What could be going on?",
+        "Reimu decided to head back to the pyramid."
+    ],
     "75": "Starlight Core",
+    "75_0": [
+        "\"This incident isn't over yet. If anything, it's only starting.\"",
+        "Okina Matara warned Reimu.",
+        "\"There's a dangerous youkai in the Fossilized Forest.\"",
+        "Reimu was about to reply 'why don't you go deal with it yourself?,",
+        "but Okina's overwhelming aura made her reluctantly set out to investigate."
+    ],
     "76": "Unholy Thunder",
+    "76_0": [
+        "This stone is imbued with Satori Komeiji's power.",
+        "It lets her read the minds of others to some extent.",
+        "Perhaps that princess babbling about strange things",
+        "was actually telling the truth.",
+        "Reimu headed for the pyramid, half believing and half disbelieving."
+    ],
     "77": "Pure Blaze",
+    "77_0": [
+        "Mulling over it carefully, Reimu concluded that this was all",
+        "the Lunarians' fault. Wanting to give them a piece of her mind,",
+        "she headed for the pyramid once again, aiming to reach the lunar ocean.",
+        "However, on the way there, the princess she ran into",
+        "told her something quite strange."
+    ],
     "78": "Mystic Mana",
+    "78_0": [
+        "\"Head to the pyramid at once, won't you?\"",
+        "Zanmu, the oni from Hell, urged Reimu on.",
+        "Apparently, there is an information-devouring monster down there.",
+        "She claims that it's unthinkable to leave that monster alone.",
+        "Having been scolded for some reason, Reimu reluctantly headed back there."
+    ],
     "79": "Otherworldy Shade",
+    "79_0": [
+        "Yuiman seemed to have visited her friend, Kanako.",
+        "She had been making complaints about some kind of monster appearing.",
+        "Since Sanae looked like she was a little hesitant,",
+        "Reimu thought she would take this opportunity to investigate.",
+        "The early bird gets the worm, after all."
+    ],
     "80": "Eternal Void",
+    "80_0": [
+        "This Incident Stone had the power of the Scarlet Devil Mansion's vampires.",
+        "Patchouli and Marisa looked into the stone together, and they found that",
+        "the stone was created solely to solve the incident.",
+        "The Incident Stones are only growing more powerful.",
+        "This could only mean that the incident has yet to end."
+    ],
     "81": "Divine Echo",
+    "81_0": [
+        "\"I haven't had enough fighting yet.\"",
+        "Marisa felt empty-headed when holding this stone.",
+        "It was made from the youkai energy of the Keiga Clan's monarch.",
+        "It told her where an incident was occurring.",
+        "\"The Fossilized Forest? Alright, just you wait!\""
+    ],
     "82": "Soul Catalyst",
+    "82_0": [
+        "\"You're still sitting around here?\"",
+        "Said Yukari Yakumo, grabbing Marisa from behind and pulling her into a gap.",
+        "\"So sorry. You're more suitable for this role than Reimu.\""
+    ],
     "83": "Starlight Core",
+    "83_0": [
+        "\"This incident isn't over yet. If anything, it's only starting.\"",
+        "Okina Matara warned Marisa.",
+        "\"There's a dangerous youkai in the Fossilized Forest.\"",
+        "Hearing Okina's strangely serious plea,",
+        "Marisa braced herself for the challenge ahead."
+    ],
     "84": "Unholy Thunder",
+    "84_0": [
+        "For some reason, Koishi Komeiji could be perceived near the Sanctuary.",
+        "\"You can see me, right? I think that means something bad might be going on.\"",
+        "Marisa had never received such a warning from Koishi, of all people,",
+        "so she headed for the pyramid with skepticism."
+    ],
     "85": "Pure Blaze",
+    "85_0": [
+        "The \"god who's skilled at crafting jewels\" that Eirin mentioned before",
+        "turned out to be Misumaru Tamatsukuri.",
+        "Upon meeting and hearing to what she had to say,",
+        "Marisa realized that Eirin had been hiding something important.",
+        "The Incident Stones would really come in handy from now on."
+    ],
     "86": "Otherworldly Shade",
-    "87": "Mystic Mana"
+    "86_0": [
+        "This stone held the evil aura of Zanmu, an oni from Hell.",
+        "In exchange for letting Marisa use the stone to solve the incident,",
+        "Zanmu had a brutal request for her.",
+        "\"There was a deity slumbering within the depths of that pyramid, wasn't there?",
+        "Bring her to me. This is an order.\""
+    ],
+    "87": "Mystic Mana",
+    "87_0": [
+        "Marisa had received some shocking news.",
+        "It turned out that Sanae had gotten into terrible trouble inside the pyramid!",
+        "Marisa felt responsible for egging her on to head in there.",
+        "But what kind of enemy could have beaten her up?",
+        "So she thought, as she challenged the pyramid once more."
+    ]
 }
